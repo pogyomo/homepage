cb54d75c985dfda87a065997087c1fe2524570f0
pogyomo <pogyomo@gmail.com>
2024-08-26 00:41:57 +0900

    refactor: use mb to add spaces after header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 const Header: React.FC = ({}) => {
   return (
     <div>
-      <header className="p-6 text-2xl justify-center bg-sky-300 text-center">
+      <header className="p-6 mb-4 text-2xl justify-center bg-sky-300 text-center">
         <p>pogyomo's homepage</p>
       </header>
-      <p className="m-4"></p>
     </div>
   );
 };
