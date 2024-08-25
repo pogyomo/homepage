28c139d151c6ab4823dcf01838da080aaf07c85a
pogyomo <pogyomo@gmail.com>
2024-08-26 01:10:19 +0900

    refactor: simplify css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header: React.FC = ({}) => {
   return (
     <div>
-      <header className="p-6 mb-4 text-2xl justify-center bg-sky-300 text-center">
-        <p>pogyomo's homepage</p>
+      <header className="p-6 mb-4 bg-sky-300">
+        <p className="text-2xl text-center">pogyomo's homepage</p>
       </header>
     </div>
   );
