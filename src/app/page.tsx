ee332c51568f95440f484a099d6b4a2eba60b86e
pogyomo <pogyomo@gmail.com>
2024-08-26 02:59:02 +0900

    fix: use text-2xl to h1 and use font-bold to h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-center mb-4">TopPage</h1>
+      <h1 className="text-2xl text-center mb-4">TopPage</h1>
       <div className="grid grid-cols-5">
         <Link href="./profile" className={boxStyle}>
           profile
