9434203873027567cef3fa8305caf5180b7daaca
pogyomo <pogyomo@gmail.com>
2024-08-26 00:31:49 +0900

    refactor: create lib to put boxStyle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,22 @@
 import Link from "next/link";
 import TodoButton from "@/components/TodoButton";
 import type { Metadata } from "next";
+import boxStyle from "@/lib/boxStyle";
 
 export const metadata: Metadata = {
   title: "toppage",
 };
 
-const boxStyle: string = `
-rounded-md
-m-2
-p-2
-border-2
-border-black
-bg-sky-300
-hover:bg-sky-500
-transition-colors
-ease-out
-duration-200
-inline-block
-cursor-pointer
-text-center
-`;
-
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-5">
-        <div className={boxStyle}>
-          <Link href="./links" className="block text-black no-underline">
-            links
-          </Link>
-        </div>
-        <div className={boxStyle}>
-          <Link href="./works" className="block text-black no-underline">
-            works
-          </Link>
-        </div>
+        <Link href="./links" className={boxStyle}>
+          links
+        </Link>
+        <Link href="./works" className={boxStyle}>
+          works
+        </Link>
         <TodoButton className={boxStyle}>todo</TodoButton>
         <TodoButton className={boxStyle}>todo</TodoButton>
         <TodoButton className={boxStyle}>todo</TodoButton>
---
+++ src/lib/boxStyle.tsx
@@ -0,0 +1,19 @@
+export const boxStyle: string = `
+rounded-md
+m-2
+p-2
+border-2
+border-black
+bg-sky-300
+hover:bg-sky-500
+transition-colors
+ease-out
+duration-200
+inline-block
+cursor-pointer
+text-center
+text-black
+no-underline
+`;
+
+export default boxStyle;
